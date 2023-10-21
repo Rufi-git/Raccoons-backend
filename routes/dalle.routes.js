76fea9d3a9e64d833ddb10774fd2c7aff25c766f
Rufi-git <rufialiyev2005@gmail.com>
2023-10-21 12:02:13 -0700

    racc backend

---
+++ routes/dalle.routes.js
@@ -0,0 +1,30 @@
+import express from "express"
+import * as dotenv from "dotenv"
+import { OpenAI } from "openai"
+
+dotenv.config();
+
+const router = express.Router()
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const response = await openai.images.generate({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json"
+        })
+        const image = response.data[0].b64_json
+        res.status(200).json({ photo: image })
+    } catch (error) {
+        console.error('OpenAI API request failed:', error.message);
+        res.status(500).json(error.message)
+    }
+})
+
+export default router;
